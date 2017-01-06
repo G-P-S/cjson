2e0c4d6372ce236ccae75688e23f5db66503d73f
Richard Navarro <rnavarro@gopro.com>
2017-01-06 11:36:31 -0800

    Increase precision of floating point printing

--- cJSON.c
+++ cJSON.c
@@ -198,7 +198,7 @@ static char *print_number(cJSON *item,printbuffer *p)
 		{
 			if (fabs(floor(d)-d)<=DBL_EPSILON && fabs(d)<1.0e60)sprintf(str,"%.0f",d);
 			else if (fabs(d)<1.0e-6 || fabs(d)>1.0e9)			sprintf(str,"%e",d);
-            else												sprintf(str,"%.14f",d);
+            else												sprintf(str,"%.20f",d);
 		}
 	}
 	return str;
